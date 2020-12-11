383134e7bc1616efc5dea5a06931d468c9c0600d
Priyadarshiswain <priyadarshiswain@outlook.com>
2020-12-11 17:21:44 +0530

    comit2

---
+++ SpaceMngSystem/Hubs/SmsHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SpaceMngSystem.Models;
+
+namespace SpaceMngSystem.Hubs
+{
+    public class SmsHub : Hub
+    {
+        public async Task Publish(PublishModel request)
+        {
+            await Clients.Group(request.PublisherId).SendAsync("Publish", request.LocationId);
+        }
+
+        public Task Subscribe(string publisherId)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, publisherId);
+        }
+
+        public Task UnSubscribe(string publisherId)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, publisherId);
+        }
+    }
+}
---
+++ SpaceMngSystem/Models/PublishModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceMngSystem.Models
+{
+    public class PublishModel
+    {
+        public string PublisherId { get; set; }
+        public object LocationId { get; set; }
+    }
+}
